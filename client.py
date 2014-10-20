f284feac792656d420b96e39962f7f52a0779145
Marco Tejedor González <m.tejedorg@alumnos.urjc.es>
2014-10-20 10:07:13 +0200

    Implementado punto 4. Ahora la IP, el puerto y el mensaje se pasan como parámetros. Indica cómo ha de usarse si se usa mal

--- client.py
+++ client.py
@@ -5,28 +5,35 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+com = sys.arg
+if len(com) = 3:
+    SERVER = com(1)
+    PORT = com(2)
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+    # Contenido que vamos a enviar
+    LINE = com(3)
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+    print "Enviando: " + LINE
+    my_socket.send(LINE + '\r\n')
+    data = my_socket.recv(1024)
 
-print 'Recibido -- ', data
-print "Terminando socket..."
+    print 'Recibido -- ', data
+    print "Terminando socket..."
 
-# Cerramos todo
-my_socket.close()
-print "Fin."
+    # Cerramos todo
+    my_socket.close()
+    print "Fin."
+
+else:
+    print "Usage: $python client.py ip puerto linea"
+       
