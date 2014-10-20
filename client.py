a8e019cb6aa972569c33f67d75aad014d9ba98dd
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-10-20 15:22:40 +0200

    ejercicio 6 el cliente manda registrer

--- client.py
+++ client.py
@@ -11,14 +11,17 @@ import sys
 # Dirección IP, puerto y mensaje para servidor
 SERVER = sys.argv[1]      
 PORT = int(sys.argv[2])
-LINE = sys.argv[3]
+METODO = sys.argv[3]
+USUARIO = sys.argv[4]
+
+client_address = (sys.argv[1],int(sys.argv[2]))
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-
+LINE = METODO.upper() + " sip:"+ USUARIO + " SIP/1.0\r\n\r\n"
 
 print "Enviando: " + LINE
 my_socket.send(LINE + '\r\n')
