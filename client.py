ec88fda1ca6b61ec541b6aadea2c892e743ef7bb
Marco Tejedor González <m.tejedorg@alumnos.urjc.es>
2014-10-20 10:29:54 +0200

    Ahora se puede introducir el mensaje en varios parámetros (no hace falta introducirlo en string) sigue dando error de uso pero es un poco feo

--- client.py
+++ client.py
@@ -10,30 +10,35 @@ import sys
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-com = sys.argv
-if len(com) == 4:
-    SERVER = com[1]
-    PORT = int(com[2])
+coms = sys.argv
+try:
+    SERVER = coms[1]
+    PORT = int(coms[2])
 
     # Contenido que vamos a enviar
-    LINE = com[3]
-
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
-
-    print "Enviando: " + LINE
-    my_socket.send(LINE + '\r\n')
-    data = my_socket.recv(1024)
-
-    print 'Recibido -- ', data
-    print "Terminando socket..."
-
-    # Cerramos todo
-    my_socket.close()
-    print "Fin."
-
-else:
+    LINE = ""
+    for com in coms[3:]:
+        LINE += com + " "
+except IndexError:
     print "Usage: $python client.py ip puerto linea"
+    sys.exit()
+
+# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+my_socket.connect((SERVER, PORT))
+
+print "Enviando: " + LINE
+my_socket.send(LINE + '\r\n')
+data = my_socket.recv(1024)
+
+print 'Recibido -- ', data
+print "Terminando socket..."
+
+# Cerramos todo
+my_socket.close()
+print "Fin."
+
+
+
        
