c9f682cd9e87dc02cc0c36dc610e25c5e5dcbdbf
Ubuntu-14.04.1 <a.oterinoc@alumnos.urjc.es>
2014-10-23 19:47:28 +0200

    ejercicio 8 a falta de acabarla gestion de caducidad

--- server.py
+++ server.py
@@ -7,12 +7,21 @@ en UDP simple
 
 import SocketServer
 import sys
-
+import time
+registro = {}
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
+    def register2file(self):
+        fich = open("registered.txt" ,"w") #abro fichero para escribir 
+        fich.write("User" + "\t" + "IP" + "\t" + "Expires" + "\r\n")
+        for clave in registro:            
+            hora = time.strftime('%Y-%m-%d %H:%M:%S' , time.gmtime(time.time()))
+            fich.write (clave + "\t" + registro[clave][0] + "\t" + hora +"\r\n")
+        fich.close()
 
+            
     def handle(self):
         #Se ejecuta cada vez que recibimos una peticion del servidor
         # Escribe dirección y puerto del cliente (de tupla client_address)
@@ -24,14 +33,29 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 break  
             print "El cliente "+ str(self.client_address) + " nos manda " + line
             line = line.split() #troceo linea que me llega
-            registro = {}
-            registro[str(self.client_address[0])] = line[1]
-            print registro
-         
-            
-            if line [0] == "REGISTER" and line [2] == "SIP/1.0":
-                self.wfile.write(" SIP/1.0 200 OK\r\n\r\n")
-                   
+            print line
+
+ 
+            if line [0] == "REGISTER" and line [2] == "SIP/1.0": #me llega un registrer mando 200 OK
+                
+                    self.wfile.write(" SIP/1.0 200 OK\r\n\r\n")
+                    
+            line[1] = line[1].split(":") #quiero el correo sin sip corto antes para poder usar line[1][1]
+            if line[4] != "0" : #si el expires es 0 no le meto en el diccionario
+                registro[line[1][1]] = [str(self.client_address[0]),line[4]] #tenemos un diccionario con valor una lsta/
+                
+                #llamada a la funcion para el FICHERO
+                #llamo a la funcion y la creo arriba.
+                self.register2file()
+
+                print registro       
+            if line[4] == "0":
+                if line[1][1] in registro:
+          
+                    print line[1][1], "a"
+                    del registro[line[1][1]] #borro del diccionario
+                    self.wfile.write(" SIP/1.0 200 OK\r\n\r\n")
+                    print registro
             
 
 if __name__ == "__main__":
