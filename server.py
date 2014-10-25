9e05f30704e71010aa1a7eb1ff52432146dc1bc7
mcapitan <miriamcapitan@gmail.com>
2014-10-25 19:48:59 +0200

    Añado ejercicio 6

--- server.py
+++ server.py
@@ -8,25 +8,36 @@ en UDP simple
 import SocketServer
 import sys
 
-
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
+    dicc = {}
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        print (sys.argv[1], int(sys.argv[2]))
+        IP = self.client_address[0]
+        PUERTO = str(self.client_address[1])
+        print "IP: " + IP + " PUERTO: " + PUERTO
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
-            self.wfile.write(line)
-            if not line:
+            lista = line.split()
+
+            #Guarda la información registrada y la IP en un diccionario
+            if lista[0] == "REGISTER":
+                usuario = lista[1][4:]
+                self.dicc[usuario] = IP
+                self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
+
+            else:
+                pass
+            if not line or "[""]":
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer((sys.argv[1], int(sys.argv[2])), EchoHandler)
+    serv = SocketServer.UDPServer(("", int(sys.argv[1])), SIPRegisterHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
