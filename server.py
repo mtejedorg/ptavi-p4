4ae44f9274eb14ca7471c54d3f3b3bd1aa9e0784
mcapitan <miriamcapitan@gmail.com>
2014-10-27 19:10:32 +0100

    Añado ejercicio 8

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
+
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
@@ -15,6 +17,27 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     dicc = {}
 
     def handle(self):
+
+        def register2file():
+            fichero = open("registered.txt", 'w')
+            fichero.write("User \t IP \t Expires \n")
+            lista = []
+            for name in self.dicc:
+                if time.time() > self.dicc[name][1]:
+                    lista.append(name)
+            for name in lista:
+                del self.dicc[name]
+
+            for clave in self.dicc:
+                expire = time.strftime('%Y-­%m-­%d %H:%M:%S', time.gmtime
+                                       (float((self.dicc[clave][1]))))
+                fichero.write(clave)
+                fichero.write("\t")
+                fichero.write(self.dicc[clave][0])
+                fichero.write(expire)
+                fichero.write("\r\n")
+            fichero.close()
+
         # Escribe dirección y puerto del cliente (de tupla client_address)
         IP = self.client_address[0]
         PUERTO = str(self.client_address[1])
@@ -28,16 +51,16 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
             #Guarda la información registrada y la IP en un diccionario
             if lista[0] == "REGISTER":
                 usuario = lista[1][4:]
-                expires = int(lista[4])
+                expires = time.time() + int(lista[4])
                 if expires == 0:
                     self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
                     if usuario in self.dicc:
                         del self.dicc[usuario]
-                        #print self.dicc[usuario]
+                        register2file()
                 if expires > 0:
                     self.dicc[usuario] = [IP, expires]
                     self.wfile.write("SIP/2.0 200 OK\r\n\r\n")
-                    #print self.dicc[usuario]
+                    register2file()
 
             else:
                 pass
